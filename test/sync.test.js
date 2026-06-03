2824d064d3972cdc6d2d645a321eee0ac0ed6a4b
doge-liang <liangsycmail@gmail.com>
2026-06-03 12:51:38 +0800

    feat(sync): add fs utils (atomic write, backup, copyDir)

--- test/sync.test.js
+++ test/sync.test.js
@@ -4,6 +4,12 @@ const assert = require('node:assert');
 const os = require('os');
 const path = require('path');
 const { expandPath } = require('../lib/paths');
+const fs = require('fs');
+const { readIfExists, atomicWrite, copyDir } = require('../lib/fsutil');
+
+function tmpdir() {
+  return fs.mkdtempSync(path.join(os.tmpdir(), 'cct-'));
+}
 
 const HOME = os.homedir();
 
@@ -19,4 +25,31 @@ test('expandPath: $HOME / ${HOME}', () => {
 });
 test('expandPath: 绝对路径原样', () => {
   assert.strictEqual(expandPath(path.normalize('/tmp/x')), path.normalize('/tmp/x'));
+});
+
+test('readIfExists: 不存在返回 null', () => {
+  assert.strictEqual(readIfExists(path.join(tmpdir(), 'nope')), null);
+});
+test('atomicWrite: 写入新文件', () => {
+  const d = tmpdir();
+  const f = path.join(d, 'a.txt');
+  atomicWrite(f, 'hello');
+  assert.strictEqual(fs.readFileSync(f, 'utf8'), 'hello');
+});
+test('atomicWrite: 覆盖前备份 .bak', () => {
+  const d = tmpdir();
+  const f = path.join(d, 'a.txt');
+  atomicWrite(f, 'old');
+  atomicWrite(f, 'new');
+  assert.strictEqual(fs.readFileSync(f, 'utf8'), 'new');
+  assert.strictEqual(fs.readFileSync(f + '.bak', 'utf8'), 'old');
+});
+test('copyDir: 递归拷贝', () => {
+  const d = tmpdir();
+  const src = path.join(d, 'src');
+  fs.mkdirSync(path.join(src, 'sub'), { recursive: true });
+  fs.writeFileSync(path.join(src, 'sub', 'x.txt'), 'X');
+  const dest = path.join(d, 'dest');
+  copyDir(src, dest);
+  assert.strictEqual(fs.readFileSync(path.join(dest, 'sub', 'x.txt'), 'utf8'), 'X');
 });
